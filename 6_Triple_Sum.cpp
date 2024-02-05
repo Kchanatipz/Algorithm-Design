d7f4f17823062e1c7d865edf2a078bc67d835bfd
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-02-05 21:49:24 +0700

    do 6 (40%)

--- 6_Triple_Sum.cpp
+++ 6_Triple_Sum.cpp
@@ -1,48 +1,44 @@
 // 6 Triple Sum
 #include <algorithm>
 #include <iostream>
+#include <map>
+#include <set>
 #include <vector>
 
 using namespace std;
 
-vector<int> v;
-vector<bool> used;
-int n, m, x;
-
-void check(auto v) {
-    for (auto &e : v) cout << e << " ";
-    cout << "\n";
-}
-
-int recur(int count, int acc) {
-    cout << acc << endl;
-    if (count == 3) {
-        return acc;
-    } else {
-        for (int i = 0; i < n; i++) {
-            if (!used[i]) {
-                used[i] = true;
-                recur(count + 1, acc + v[i]);
-                used[i] = false;
-            }
-        }
-    }
-}
-
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n, m, x;
+    vector<int> v;
+    map<int, pair<int, int>> sum;
+    bool check = true;
     cin >> n >> m;
     for (int i = 0; i < n; i++) {
         cin >> x;
         v.push_back(x);
     }
-    used.resize(n);
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (v[i] != v[j]) {
+                sum[v[i] + v[j]] = {v[i], v[j]};
+            }
+        }
+    }
     for (int i = 0; i < m; i++) {
         cin >> x;
-        // cout << ((recur(0, 0) == x) ? "YES" : "NO") << "\n";
-        cout << "ANS : " << recur(0, 0) << endl;
+        check = false;
+        for (auto &[add, value] : sum) {
+            for (auto &e : v) {
+                if (add + e == x && value.first != e && value.second != e) {
+                    check = true;
+                    break;
+                }
+            }
+        }
+        cout << ((check) ? "YES" : "NO") << "\n";
     }
-    recur(0, 0);
-    check(v);
     return 0;
 }
 
