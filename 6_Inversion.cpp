8cc91f2a95a2dcf4dc85bdd57fa6d50487cefd4b
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-02-05 13:15:45 +0700

    done 6

--- 6_Inversion.cpp
+++ 6_Inversion.cpp
@@ -6,17 +6,33 @@
 using namespace std;
 
 int main() {
-    int n, m, ans = 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n, ans = 0;
     cin >> n;
-    vector<int> v(n);
+    int v[n];
     for (int i = 0; i < n; i++) {
         cin >> v[i];
     }
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
-            if (v[i] > v[j]) ans++;
+            if (v[i] > v[j]) {
+                ans++;
+            }
         }
     }
+    // int i = 0;
+    // int j = 1;
+    // while (true) {
+    //     if (i == n - 2) break;
+    //     if (j == n) {
+    //         i++;
+    //         j = i + 1;
+    //     }
+    //     cout << v[i] << " " << v[j] << " " << (v[i] > v[j]) << "\n";
+    //     if (v[i] > v[j]) ans++;
+    //     j++;
+    // }
     cout << ans << "\n";
     return 0;
 }
