7ab81943fbad37e01913bd14244627361ac7185a
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-02-09 22:56:50 +0700

    do 9 (90%)

--- 12_Virus_2.cpp
+++ 12_Virus_2.cpp
@@ -4,7 +4,18 @@
 
 using namespace std;
 
-bool recur() {
+bool recur(vector<int> v, int start, int len) {
+    if (len == 4) return abs(v[start] + v[start + 1] - v[start + 2] - v[start + 3]) <= 1;
+    int diff = 0;
+    len /= 2;
+    for (int i = 0; i < len; i++) {
+        // cout << start + i << " " << start + len + i << "\n";
+        diff += v[start + i];
+        diff -= v[start + len + i];
+    }
+    // cout << diff << "\n";
+    if (abs(diff) > 1) return false;
+    return recur(v, start, len) && recur(v, start + len, len);
 }
 
 int main() {
@@ -17,11 +28,24 @@ int main() {
         for (int i = 0; i < pow(2, k); i++) {
             cin >> v[i];
         }
-        if (recur()) {
-            cout < "yes\n";
+        if (recur(v, 0, pow(2, k))) {
+            cout << "yes\n";
         } else {
             cout << "no\n";
         }
     }
     return 0;
 }
+
+/*
+4 3
+0 0 1 1 0 0 1 1
+0 0 1 1 1 0 0 0
+0 1 0 1 0 1 1 1
+0 1 0 1 1 1 0 0
+
+3 4
+1 1 1 1 1 1 1 0 0 1 1 1 1 1 0 0
+1 1 1 1 1 1 1 0 0 1 1 1 1 1 0 1
+1 1 1 0 0 1 1 1 1 0 0 1 1 0 1 1
+*/
