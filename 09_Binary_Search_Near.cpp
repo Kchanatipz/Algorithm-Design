9d694c3e49dde340fc40a1571f2b051c58dc659c
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-02-10 10:27:22 +0700

    9: delete comments and optimize code

--- 09_Binary_Search_Near.cpp
+++ 09_Binary_Search_Near.cpp
@@ -6,22 +6,16 @@ vector<int> v;
 int n, m, value;
 
 int recur(int start, int stop) {
-    // cout << start << " " << stop << "\n";
     if (value < v[0]) return -1;
     if (value > v[v.size() - 1]) return v[v.size() - 1];
     if (start == stop) {
-        // if ((start == 0 || start == n - 1) & v[start] <= value) return v[start];
         if (value == v[start]) return v[start];
-        if (value < v[start]) return v[start - 1];
-        return -1;
+        return v[start - 1];
     }
     int mid = (start + stop) / 2;
-    // cout << mid;
     if (v[mid] < value) {
-        // cout << " right\n";
         return recur(mid + 1, stop);
     } else {
-        // cout << " left\n";
         return recur(start, mid);
     }
 }
@@ -33,23 +27,8 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> v[i];
     }
-    for (int i = 0; i < m; i++) {
+    while (m--) {
         cin >> value;
-        int ans = -1;
         cout << recur(0, n - 1) << "\n";
     }
 }
-
-/*
-10 20
-1 5 7 9 16 20 22 25 31 35
-38 34 20 30 33 12 7 27 28 16 39 39 14 12 5 21 24 33 31 0
-
-10 3
-1 5 7 9 16 20 22 25 31 35
-30 33 12
-
-4 7
-14 15 20 30
-10 11 14 15 16 21 68
-*/
