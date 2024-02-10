3d636e8e4ef3f723c2cc8e5c5ecbfa9b5a6500b9
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-02-10 22:29:55 +0700

    fix 13

--- 13_Tiling.cpp
+++ 13_Tiling.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 using namespace std;
-int l, x, y;
+int l, a, b;
 vector<vector<int>> v;
 vector<int> u;
 
@@ -16,37 +16,42 @@ void check() {
 void fill(int a, int b) {
     if (v[a][b] & !v[a + 1][b] & !v[a][b + 1] & !v[a + 1][b + 1]) {
         v[a + 1][b] = v[a][b + 1] = v[a + 1][b + 1] = 1;
-        cout << "1 " << a << " " << b << "\n";
+        cout << "ANS : 0 " << a << " " << b << "\n";
     } else if (!v[a][b] & v[a + 1][b] & !v[a][b + 1] & !v[a + 1][b + 1]) {
         v[a][b] = v[a][b + 1] = v[a + 1][b + 1] = 1;
-        cout << "2 " << a << " " << b << "\n";
+        cout << "ANS : 1 " << a << " " << b << "\n";
     } else if (!v[a][b] & !v[a + 1][b] & v[a][b + 1] & !v[a + 1][b + 1]) {
         v[a][b] = v[a + 1][b] = v[a + 1][b + 1] = 1;
-        cout << "3 " << a << " " << b << "\n";
+        cout << "ANS : 2 " << a << " " << b << "\n";
     } else {
         v[a][b] = v[a + 1][b] = v[a][b + 1] = 1;
-        cout << "4 " << a << " " << b << "\n";
+        cout << "ANS : 3 " << a << " " << b << "\n";
     }
+    check();
 }
 
 void recur(int x, int y, int len) {
-    cout << x << " " << y << " " << len << "\n";
+    // cout << "@ " << x << " " << y << " " << len << "\n";
     if (len == 2) {
+        cout << x << " " << y << "\n";
+        fill(x, y);
         return;
     }
     len /= 2;
-    recur();
-    recur();
+    recur(x, y, len);
+    recur(x, y + len, len);
+    recur(x + len, y, len);
+    recur(x + len, y + len, len);
 }
 
 int main() {
-    cin >> l >> x >> y;
+    cin >> l >> a >> b;
     v.resize(l);
     u.resize(l);
     for (int i = 0; i < l; i++) {
         v[i] = u;
     }
-    recur(0, l);
-    check();
+    v[a][b] = 1;
+    recur(0, 0, l);
     return 0;
 }
