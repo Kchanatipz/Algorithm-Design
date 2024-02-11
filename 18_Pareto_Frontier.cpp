78d9d572e2fcb9bfaed395cb056a63227d961850
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-02-11 17:24:28 +0700

    add 18 (not done)

--- 18_Pareto_Frontier.cpp
+++ 18_Pareto_Frontier.cpp
@@ -2,7 +2,30 @@
 #include <vector>
 
 using namespace std;
+vector<pair<int, int>> v;
+int n, ans = 0;
+
+void recur(int start, int stop, int minY) {
+    if (start == stop) {
+        if (v[start].second < minY) {
+            ans++;
+        }
+        return;
+    }
+    int mid = (start + stop) / 2;
+    recur(start, mid, v[start].second);
+    recur(mid + 1, stop, v[mid + 1].second);
+}
 
 int main() {
+    cin >> n;
+    v.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i].first;
+        cin >> v[i].second;
+    }
+    sort(v.begin(), v.end());
+    recur(0, n, 0);
+    cout << ans << "\n";
     return 0;
 }
