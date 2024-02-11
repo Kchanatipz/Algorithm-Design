b8bb01355a7127dcee58cf493508a7e44c31d36c
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-02-11 23:24:07 +0700

    add 20 (not done)

---
+++ 20_Law_Mowing.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int n, m, k;
+vector<int> v;
+
+int recur(int start) {
+    return 0;
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    cin >> n >> m >> k;
+    v.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    while (n--) {
+    }
+    return 0;
+}
