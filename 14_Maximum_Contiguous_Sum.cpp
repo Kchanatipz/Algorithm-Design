f7165b66f21ac1a25d229526b6489e4300d22dcd
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-02-11 11:10:41 +0700

    fix 14

--- 14_Maximum_Contiguous_Sum.cpp
+++ 14_Maximum_Contiguous_Sum.cpp
@@ -2,34 +2,40 @@
 #include <vector>
 
 using namespace std;
-vector<int> v;
-vector<int> sum;
+vector<int> v, s;
 int n;
 
 int recur(int start, int stop) {
     if (start == stop) return v[start];
     int mid = (start + stop) / 2;
-    int max_left = recur(start, mid);
-    int max_right = recur(mid + 1, stop);
-    // int diff, max_all = 0;
-    // for (int i = start; i < stop; i++) {
-    //     diff = sum[stop] - sum[start];
-    //     if (diff > max_all) max_all = diff;
-    // }
-    return max(max_left, max_right);
+
+    int r1 = recur(start, mid);
+    int r2 = recur(mid + 1, stop);
+    cout << "-------------\n";
+    int sum, r3 = v[start];
+    for (int i = 1; i < mid; i++) {
+        for (int j = mid + 1; j < n; j++) {
+            sum = s[j] - s[i];
+            cout << i << " " << j << " " << s[j] << " " << s[i] << " " << sum << "\n";
+            if (sum > r3) r3 = sum;
+        }
+    }
+    cout << start << " " << stop << "\n";
+    cout << "-- " << r1 << " " << r2 << " " << r3 << "\n";
+    cout << "-------------\n";
+    return max(max(r1, r2), r3);
 }
 
 int main() {
     cin >> n;
-    v.resize(n);
-    sum.resize(n);
-    for (int i = 0; i < n; i++) {
+    v.resize(n + 1);
+    s.resize(n + 1, 0);
+    for (int i = 1; i <= n; i++) {
         cin >> v[i];
+        s[i] += s[i - 1] + v[i];
     }
-    sum[0] = v[0];
-    for (int i = 1; i < n; i++) {
-        sum[i] += sum[i - 1] + v[i];
-    }
+    s[0] = 0;
+    for (auto &e : s) cout << e << " ";
     cout << recur(0, n - 1) << "\n";
 }
 
