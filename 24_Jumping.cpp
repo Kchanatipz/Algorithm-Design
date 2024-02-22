e06d5937a6fb23e86f8fcbeebb5dd3206ed64abd
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-02-22 11:12:28 +0700

    done 24 (bottom up)

--- 24_Jumping.cpp
+++ 24_Jumping.cpp
@@ -4,30 +4,22 @@
 
 using namespace std;
 vector<int> v, score;
-vector<bool> used;
 int n;
-
-int recur(int step) {
-    if (step < 1) return -99999;
-    if (!used[step]) {
-        // cout << step << " " << one << " " << two << " " << three << "\n";
-        score[step] = max(max(recur(step - 1), recur(step - 2)), recur(step - 3)) + v[step];
-        used[step] = true;
-    }
-    return score[step];
-}
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
     cin >> n;
-    v.resize(n + 1);
-    used.resize(n + 1);
-    score = v;
-    for (int i = 1; i <= n; i++)
+    v.resize(n);
+    score.resize(n);
+    for (int i = 0; i < n; i++) {
         cin >> v[i];
-    score[1] = v[1];
-    used[1] = true;
-    recur(n);
-    cout << score[n] << "\n";
+    }
+    score[0] = v[0];
+    score[1] = v[0] + v[1];
+    score[2] = max(v[0] + v[2], v[0] + v[1] + v[2]);
+    for (int i = 3; i < n; i++) {
+        score[i] = max(max(score[i - 1], score[i - 2]), score[i - 3]) + v[i];
+    }
+    cout << score[n - 1] << "\n";
     return 0;
 }
