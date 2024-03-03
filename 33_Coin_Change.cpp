891d6c3819c2c5a5302f7491c8a41d5baefd2aa7
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-03-03 10:12:00 +0700

    done 33

--- 33_Coin_Change.cpp
+++ 33_Coin_Change.cpp
@@ -3,14 +3,7 @@
 #include <vector>
 
 using namespace std;
-
-void check(vector<vector<int>> v) {
-    cout << ">-<\n";
-    for (auto &i : v) {
-        for (auto &j : i) cout << j << " ";
-        cout << "\n";
-    }
-}
+const int LARGE = 1e9 + 7;
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
@@ -18,19 +11,26 @@ int main() {
     int n, k;
     cin >> n >> k;
 
-    vector<int> coin(n + 1);
-    vector<vector<int>> dp(n + 1, vector<int>(k + 1));
-    for (int i = 1; i <= n; i++) {
+    vector<int> coin(n), dp(k + 1, LARGE);
+    for (int i = 0; i < n; i++) {
         cin >> coin[i];
     }
-    check(dp);
-    cout << dp[n][k] << "\n";
+
+    dp[0] = 0;
+    for (int i = 1; i <= k; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i >= coin[j]) {
+                dp[i] = min(dp[i], dp[i - coin[j]] + 1);
+            }
+        }
+    }
+    cout << dp[k] << "\n";
     return 0;
 }
 
 /*
 4 28
-5 10 5 2 1
+10 5 2 1
 = 5
 
 3 13
