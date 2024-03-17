0a85709a6c5c0c4112264ab1dfd801022fcf9868
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-03-17 21:01:34 +0700

    fix 303

--- 303_Number_Triangle.cpp
+++ 303_Number_Triangle.cpp
@@ -5,16 +5,17 @@
 using namespace std;
 
 int main() {
-    int n, ans = -1;
-    cin.tie(0)->sync_with_stdio(false);
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    int n, ans = -(1e9 + 7);
     cin >> n;
-    vector<vector<int>> v(n + 1, vector<int>(n + 1));
-    cin >> v[1][1];
-    for (int i = 2; i <= n; i++) {
-        for (int j = 1; j < i + 1; j++) {
-            cin >> v[i][j];
-            v[i][j] = max(v[i - 1][j - 1], v[i - 1][j]) + v[i][j];
-            ans = max(ans, v[i][j]);
+
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1));
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cin >> dp[i][j];
+            dp[i][j] += max(dp[i - 1][j - 1], dp[i - 1][j]);
+            ans = max(ans, dp[i][j]);
         }
     }
     cout << ans << "\n";
