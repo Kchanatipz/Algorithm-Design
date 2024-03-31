2b5987e60d38b5fe4fe4e56a858f86d8cdd87a44
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-03-31 17:06:52 +0700

    not done 515

--- 515_Mass_Relay.cpp
+++ 515_Mass_Relay.cpp
@@ -4,7 +4,21 @@
 using namespace std;
 
 int main() {
-    cin.tie(nullptr)->sync_eith_stdio(false);
+    cin.tie(nullptr)->sync_with_stdio(false);
 
+    int n, m, q, a, b, c;
+    cin >> n >> m >> q;
+    vector<vector<int>> graph(n);
+    map<pair<int, int>, int> cost;
+
+    while (m--) {
+        cin >> a >> b > c;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+        cost[{a, b}] = c;
+    }
+    while (q--) {
+        cin >> a;
+    }
     return 0;
 }
